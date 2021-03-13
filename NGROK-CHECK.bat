0eed0d70f30470536cc7884efacd1b4942bd5bb2
ngoc7sao9 <27774949+ngoc7sao9@users.noreply.github.com>
2021-03-14 02:25:21 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -21,9 +21,7 @@ CD /D "%~dp0"
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator 01217682717Huy /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user VssAdministrator 01217682717Huy /add >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
 echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
@@ -39,6 +37,6 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit
-echo User: administrator
+echo User: VssAdministrator
 echo Pass: 01217682717Huy
 ping -n 99999999999999999999 127.0.0.1 >nul
