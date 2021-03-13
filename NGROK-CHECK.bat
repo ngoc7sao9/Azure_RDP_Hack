d340d7c9227cef01fbe6bc233b300f98ad9d14d5
ngoc7sao9 <27774949+ngoc7sao9@users.noreply.github.com>
2021-03-14 01:22:35 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -18,7 +18,7 @@ pushd "%CD%"
 CD /D "%~dp0"
 :: BatchGotAdmin (Run as Admin code ends)
 :: Your codes should start from the following line
-del /f "C:\Users\administrator\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator 01217682717Huy /add >nul
@@ -29,7 +29,7 @@ echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada 
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
 net user installer /delete
 curl -o "C:\Windows\System32\Drivers\etc\hosts" https://raw.githubusercontent.com/ztvofficial/Windows2019RDP-US/main/Files/hosts > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\a.ps1" https://raw.githubusercontent.com/ngoc7sao9/Azure_RDP_Hack/main/a.ps1 > out.txt 2>&1
+curl -o "C:\Users\administrator\Desktop\a.ps1" https://raw.githubusercontent.com/ngoc7sao9/Azure_RDP_Hack/main/a.ps1 > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "C:\Users\Public\Desktop\a.ps1" > out.txt 2>&1
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
